dcf6ddefa4727dd50910b11398464718352f084b
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2021-08-23 21:41:13 -0300

    partial client.py

--- Pg. Mobile/backend_pokeinfo/version_01/client.py
+++ Pg. Mobile/backend_pokeinfo/version_01/client.py
@@ -1,14 +1,46 @@
+import urllib.request
+import json
+import time
+import pandas as pd
+
+# pokemon routes
+URL_SERVICE = "http://127.0.0.1:5000/"
+IS_ALIVE = URL_SERVICE + "isalive/"
+POKEMON = URL_SERVICE + "pokemon/"
+
+
+def access(url):
+    print('Accessing url: ', url)
+
+    response = urllib.request.urlopen(url)
+    # data = response.read()
+
+    print(response)
+    # return data.decode("utf-8")
+
+
 def is_alive():
-    pass
+    alive = False
+
+    if access(IS_ALIVE) == 'yes':
+        alive = True
+
+    return alive
+
+
+def get_pokemon():
+    data = access(POKEMON)
+    pokemons = json.loads(data)
+
+    return pokemons
 
 
 if __name__ == '__main__':
     # Verify if the service is available (is_alive)
     is_alive = is_alive()
 
+    print(is_alive)
     # if the service is online...
-    if is_alive:
-        pokemons = get_pokemons()
     # get the list of pokemons
     # otherwise
     # return a inactivity message
